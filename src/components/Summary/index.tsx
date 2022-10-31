5add6aba3034a1b6375ecfda76df264c101a2b73
Gustavo Salviato <75762976+gustavosalviato@users.noreply.github.com>
2022-10-31 13:27:52 -0300

    Calculando os valores das transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,26 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
+import { ArrowCircleDown, ArrowCircleUp, ArrowCounterClockwise, CurrencyDollar } from "phosphor-react"
+import { useContextTrasaction } from "../../contexts/Trasactions"
+import { formatPrice } from "../../helpers/formatPrice"
 import { SummaryCard, SummaryContainer } from "./styles"
 
 export const Summary = () => {
+    const { transactions } = useContextTrasaction()
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'income') {
+            acc.income += transaction.price
+            acc.total += transaction.price
+        } else {
+            acc.outcome += transaction.price
+            acc.total -= transaction.price
+        }
+
+        return acc
+    }, {
+        income: 0,
+        outcome: 0,
+        total: 0,
+    })
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -10,7 +29,7 @@ export const Summary = () => {
                     <ArrowCircleUp size={32} color="#00B37E" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {formatPrice(summary.income)}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -19,16 +38,16 @@ export const Summary = () => {
                     <ArrowCircleDown size={32} color="#F75A68" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {formatPrice(summary.outcome)}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
-                <header> 
+                <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#FFFFFF" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {formatPrice(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
