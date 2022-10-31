bd367ab1dcbf9110f87363b8432ca7719fd41040
Gustavo Salviato <75762976+gustavosalviato@users.noreply.github.com>
2022-10-31 16:07:48 -0300

    Hook useSummary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,26 +1,12 @@
 import { ArrowCircleDown, ArrowCircleUp, ArrowCounterClockwise, CurrencyDollar } from "phosphor-react"
 import { useContextTrasaction } from "../../contexts/Transactions"
 import { formatPrice } from "../../helpers/formatPrice"
+import { useSummary } from "../../hooks/useSummary"
 import { SummaryCard, SummaryContainer } from "./styles"
 
 export const Summary = () => {
-    const { transactions } = useContextTrasaction()
 
-    const summary = transactions.reduce((acc, transaction) => {
-        if (transaction.type === 'income') {
-            acc.income += transaction.price
-            acc.total += transaction.price
-        } else {
-            acc.outcome += transaction.price
-            acc.total -= transaction.price
-        }
-
-        return acc
-    }, {
-        income: 0,
-        outcome: 0,
-        total: 0,
-    })
+    const summary = useSummary()
 
     return (
         <SummaryContainer>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,24 @@
+import { useContextTrasaction } from "../contexts/Transactions"
+
+export const useSummary = () => {
+
+  const { transactions } = useContextTrasaction()
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  })
+
+  return summary
+}
