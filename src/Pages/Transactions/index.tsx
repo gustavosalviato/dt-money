5a9167d629d165050666246d2b635a5640a85b0a
gustavosalviato <gustavosalviato8@gmail.com>
2022-10-30 21:32:56 -0300

    Estrutura Table Transactions

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header"
 import { InputSearch } from "../../components/InputSearch"
 import { Summary } from "../../components/Summary"
-import { TransactionsContainer } from "./styles"
+import { TransactionsContainer, TableContainer, PriceHightlight } from "./styles"
 
 export const Transactions = () => {
     return (
@@ -11,6 +11,34 @@ export const Transactions = () => {
 
             <TransactionsContainer>
                 <InputSearch />
+
+
+                <TableContainer>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHightlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHightlight>
+
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Alimentação</td>
+                            <td>
+                                <PriceHightlight variant="outcome">
+                                    - R$ 12.000,00
+                                </PriceHightlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TableContainer>
             </TransactionsContainer>
 
         </div>
--- src/Pages/Transactions/styles.ts
+++ src/Pages/Transactions/styles.ts
@@ -7,3 +7,33 @@ export const TransactionsContainer = styled.main`
     margin: 6.4rem auto 0;
     padding: 0 2.4rem;
 `
+
+export const TableContainer = styled.table`
+    width: 100%;
+    margin-top: 2.4rem;
+    border-collapse: separate;
+    border-spacing: 0 0.8rem;
+
+
+    td {
+      padding: 2.4rem 3.2rem;
+        background: ${props => props.theme["gray-700"]};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+interface PriceHightlightProps {
+    variant: 'income' | 'outcome'
+}
+
+export const PriceHightlight = styled.span<PriceHightlightProps>`
+    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]}
+`
